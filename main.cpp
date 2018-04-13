c3b1cca7153c7c9f0cd5699d7b02b8ef7afc5adf
Josh Wright <madewithlinux.josh@gmail.com>
2018-04-13 17:21:15 -0500

    glut hello world

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 
+#include <GL/glut.h>
+
 using std::cout;
 using std::endl;
 
-int main(int argc, char** argv) {
-  cout << "hello world" << endl;
+
+void display();
+
+int main(int argc, char **argv) {
+    glutInit(&argc,argv);
+    cout << "hello world" << endl;
+
+    glutInitDisplayMode(GLUT_RGB);
+    glutInitWindowSize(400,400);
+    int win = glutCreateWindow("mandelbrot");
+
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1, 0, 0);
+    glBegin(GL_TRIANGLES);
+    {
+        glVertex2f(1, 0);
+        glVertex2f(0, 0);
+        glVertex2f(0, 1);
+    }
+    glEnd();
+    glutSwapBuffers();
 }
